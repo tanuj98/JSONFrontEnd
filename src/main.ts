13b94437374e66d57191aa7bc68360f0b3c6fb41
RitikaShrivastava <ritika.shrivastava@utdallas.edu>
2019-11-25 00:44:55 -0600

    Added scaffolded project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import './polyfills';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './app/material-module';
 
-if (environment.production) {
-  enableProdMode();
-}
+import {TableHttpExample} from './app/table-http-example';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  entryComponents: [TableHttpExample],
+  declarations: [TableHttpExample],
+  bootstrap: [TableHttpExample],
+  providers: []
+})
+export class AppModule {}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
